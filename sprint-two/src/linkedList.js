a7373aa7db2ab31eaf341a2628324b137ffa2c4c
ephraimg <ephraim.glick@gmail.com>
2017-10-11 16:42:07 -0700

    Complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (this.tail !== null) {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    if (this.head === null) {
+      this.head = node;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target) {    
+    var searchList = function(node) {
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return searchList(node.next);
+    };
+    return searchList(list.head);
   };
-
   return list;
 };
 
