5b554e761ac761fcfa072bb618e2123b88e79199
ephraimg <ephraim.glick@gmail.com>
2017-10-12 10:51:27 -0700

    Complete hashTable except pending tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,46 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket === undefined) {
+    this._storage.set(index, []); // [] is our bucket
+    bucket = this._storage.get(index);
+  }
+  var found = false;
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v;
+      found = true;
+    }
+  }
+  if (!found) {
+    bucket.push([k, v]); // now bucket should be [[k, v]]
+  }
+  // this._storage.set(index, bucket [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  var bucket = this._storage.get(index);
+  if (bucket !== undefined) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+      }
+    }
+  }
+  
 };
 
 
